5dea2cb5cc7978fb393e7bd3df00113beb7d9d7e
Oriol Codina Vallori <oriol.codina10@gmail.com>
2023-10-24 17:16:30 +0200

    lab done

---
+++ your-code/update.sql
@@ -0,0 +1,15 @@
+UPDATE salesperson
+SET company_store = 'Miami'
+WHERE staff_id = 5;
+
+UPDATE customer
+SET email = 'ppicasso@gmail.com'
+WHERE customer_id = 10001;
+
+UPDATE customer
+SET email = 'lincoln@us.gov'
+WHERE customer_id = 20001;
+
+UPDATE customer
+SET email = 'hello@napoleon.me'
+WHERE customer_id = 30001;
